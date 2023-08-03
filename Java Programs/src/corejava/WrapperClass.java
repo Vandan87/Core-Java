38aa671a681dd0ebf0b5b9cd2207339f8778af91
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-03 19:35:58 +0530

    Update WrapperClass.java

--- Java Programs/src/corejava/WrapperClass.java
+++ Java Programs/src/corejava/WrapperClass.java
@@ -1,30 +1,21 @@
 package corejava;
-
 /* Program aim is to compare to variable using wrapper class */
-
 public class WrapperClass {
 
 	public static void main(String args[]) {
-
 //		Variable create for integer wrapper class
 		Integer value1 = 10;
 		Integer value2 = 11;
-		
 		Double value3 = 3.14;
 		Double value4 = 3.14;
-		
 		Float value5 = 1.78f;
 		Float value6 = 1.78f;
-		
 		Character value7 = 'V';
 		Character value8 = 'V';
-		
 		String value9 = "Vandan";
 		String value10 = "Vandan";
-		
 		Boolean value11 = true;
 		Boolean value12 = true;
-
 //		Compare the value using equals() method
 		System.out.println("Integer : " + value1.equals(value2));
 		System.out.println("Double : " + value3.equals(value4));
