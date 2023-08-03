d699dab29c72226856fc85bbf19eb62e88189659
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-03 19:14:40 +0530

    Update EmiCalculator.java

--- Java Programs/src/corejava/EmiCalculator.java
+++ Java Programs/src/corejava/EmiCalculator.java
@@ -3,40 +3,29 @@ package corejava;
 /* Program is about for EMI calculator */
 
 import java.util.*;
-
 class Calculation {
-	
 	double emicalculation;
-
 //	EMI formula using interest formula	
 	Calculation(double principle, double rate, double time) {
 		emicalculation = (principle * rate * Math.pow(1 + rate , time) / (Math.pow(1 + rate , time) - 1));
 	}
 }
-
 public class EmiCalculator {
 
 	public static void main(String args[]) {
-
 //	 	Input form user
-		Scanner input = new Scanner(System.in);
-		double principle,rate,time;
-
+		Scanner scanner = new Scanner(System.in);
+		double principle, rate, time;
 		System.out.print("Enter Principle : ");
-		principle = input.nextFloat();
-
+		principle = scanner.nextFloat();
 		System.out.print("Enter Rate : ");
-		rate = input.nextFloat();
-
+		rate = scanner.nextFloat();
 		System.out.print("Enter Time(In months) : ");
-		time = input.nextFloat();
-
+		time = scanner.nextFloat();
 		rate = rate / (12 * 100); // Calculate interest rate
-
-		Calculation interestcalculation = new Calculation(principle, rate, time);
-
-		System.out.print("EMI is " + interestcalculation.emicalculation);
-		input.close();
+		Calculation interestCalculation = new Calculation(principle, rate, time);
+		System.out.print("EMI is " + interestCalculation.emicalculation);
+		scanner.close();
 	}
 
 }
