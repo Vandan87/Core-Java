251534a0781e5d282a7f551eb1ad52cf92803f03
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-03 19:34:49 +0530

    Update ThisAndSuper.java

--- Java Programs/src/corejava/ThisAndSuper.java
+++ Java Programs/src/corejava/ThisAndSuper.java
@@ -1,11 +1,7 @@
 package corejava;
-
 /* Program aim is use of this and super keyword */
-
 class ProductCategory {
-	
 	private String categoryname;
-
 	public void setdata(String categoryname) {
 		this.categoryname = categoryname;
 	}
@@ -13,17 +9,12 @@ class ProductCategory {
 	public void showdata() {
 		System.out.println("Product Category Name : " + categoryname);
 	}
-
 }
-
 class Product extends ProductCategory {
 	private String name;
 	private float price;
-
 	public void setdata(String name, float price, String categoryname) {
-		
 		super.setdata(categoryname); // set method of parent class
-		
 		this.name = name;
 		this.price = price;
 	}
@@ -31,17 +22,13 @@ class Product extends ProductCategory {
 	public void showdata() {
 		System.out.println("Product Name : " + name);
 		System.out.println("Product Price : " + price);
-
 		super.showdata(); // show data of parent class
 	}
 }
-
 class ThisAndSuper {
 	
 	public static void main(String[] args) {
-		
 		Product object = new Product();
-
 		object.setdata("KeyBoard",250f,"Computer Parts"); // set data manually 
 		object.showdata(); // calling method of product class
 	}
