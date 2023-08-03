e7ce09942c58b352c35191c5bf1168bc0e38e547
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-03 19:29:49 +0530

    Update IfElse.java

--- Java Programs/src/corejava/IfElse.java
+++ Java Programs/src/corejava/IfElse.java
@@ -1,12 +1,9 @@
 package corejava;
-
 /* Program's aim is use of if else condition */
-
 public class IfElse {
 
 	public static void main(String args[]) {
 		int age = 17;
-		
 // 		When age greater than 18 it is eligible
 		if (age > 18) {
 			System.out.println("You are eligible for job");
