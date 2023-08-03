f3386934be25201d511f8f4e8ed7c838c933a518
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-03 19:08:12 +0530

    Update BreakAndContinue.java

--- Java Programs/src/corejava/BreakAndContinue.java
+++ Java Programs/src/corejava/BreakAndContinue.java
@@ -5,14 +5,11 @@ package corejava;
 public class BreakAndContinue {
 
 	public static void main(String args[]) {
-
 		for (int number = 1; number <= 10; number++) {
-
 //			Check condition and when condition is true than loop is stop with this number
 			if (number == 8) {
 				break;
 			}
-
 //			Check condition and when condition is true it is skip this number and continue
 			if (number == 2) {
 				continue;
