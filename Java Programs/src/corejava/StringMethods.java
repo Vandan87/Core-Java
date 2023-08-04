53780a6e37d1b5896b53d13e256ef425e0a19f6b
Vandan87 <141118725+Vandan87@users.noreply.github.com>
2023-08-04 10:22:37 +0530

    Update StringMethods.java

--- Java Programs/src/corejava/StringMethods.java
+++ Java Programs/src/corejava/StringMethods.java
@@ -3,40 +3,40 @@ package corejava;
 public class StringMethods {
 
 	public static void main(String args[]) {
-		String word = "Vandan";
-		System.out.println(word.toUpperCase()); // VANDAN
-		System.out.println(word.toLowerCase()); // vandan
-		System.out.println(word.charAt(2)); // n
-		System.out.println(word.charAt(4)); // a
-		System.out.println(word.length()); // 6
-		System.out.println(word.startsWith("Va")); // true
-		System.out.println(word.endsWith("a")); // false
-		System.out.println(word.substring(3,5)); // da
-		String replaceString = word.replace("Vandan", "Vanshika");
+		String name = "Vandan";
+		System.out.println(name.toUpperCase()); // VANDAN
+		System.out.println(name.toLowerCase()); // vandan
+		System.out.println(name.charAt(2)); // n
+		System.out.println(name.charAt(4)); // a
+		System.out.println(name.length()); // 6
+		System.out.println(name.startsWith("Va")); // true
+		System.out.println(name.endsWith("a")); // false
+		System.out.println(name.substring(3,5)); // da
+		String replaceString = name.replace("Vandan", "Vanshika");
 		System.out.println(replaceString); // replace string with Vanshika
-		int index1 = word.indexOf("n");
-		int index2 = word.lastIndexOf("a");
+		int index1 = name.indexOf("n");
+		int index2 = name.lastIndexOf("a");
 		System.out.println(index1); // 2
 		System.out.println(index2); // 4
-		String word1 = "I";
-		String word2 = "Am";
-		String word3 = "Vandan";
-		String word4 = word1.concat(word2).concat(word3); // IAmVandan
-		System.out.println(word4);
-		System.out.println(word1.isEmpty()); // false
-		System.out.println(word4.contains("Vandan")); // true
-		System.out.println(word1.compareTo(word2)); // I=9 and A=1 i is 8 time grater than a
-		String word5 = "Vandan";
-		System.out.println(word1.equals(word2)); // false
-		System.out.println(word3.equals(word5)); // true
-		String word7 = " QDev ";
-		System.out.println(word7); // (space)QDev
-		System.out.println(word7.trim()); // QDev
+		String message1 = "I";
+		String message2 = "Am";
+		String message3 = "Vandan";
+		String message4 = message1.concat(message2).concat(message3); // IAmVandan
+		System.out.println(message4);
+		System.out.println(message1.isEmpty()); // false
+		System.out.println(message4.contains("Vandan")); // true
+		System.out.println(message1.compareTo(message2)); // I=9 and A=1 i is 8 time grater than a
+		String message5 = "Vandan";
+		System.out.println(message1.equals(message2)); // false
+		System.out.println(message3.equals(message5)); // true
+		String companyName = " QDev ";
+		System.out.println(companyName7); // (space)QDev
+		System.out.println(companyName7.trim()); // QDev
 		int number = 10;
-		String number1 = String.valueOf(number);
-		System.out.println(number1 + 11); // 1011
-		String joinString1 = String.join("-", "I", "Am", "Vandan");
-		System.out.println(joinString1); // I-Am-Vandan
+		String result = String.valueOf(number);
+		System.out.println(result + 11); // 1011
+		String sentence = String.join("-", "I", "Am", "Vandan");
+		System.out.println(sentence); // I-Am-Vandan
 	}
 
 }
